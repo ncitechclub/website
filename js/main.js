8f8cb25beb9139a0b1845265e8129e0ee14dbd9c
Lianne <ideng0212@gmail.com>
2023-12-25 17:02:05 +0000

    update navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-//main.js
+//navbar.js
+$(function () {
+    function checkWidth() {
+        if ($(window).width() <= 990) {
+            $('.navbar').addClass('navbar-dark');
+        } else {
+            $('.navbar').removeClass('navbar-dark');
+        }
+    }
+
+    // Execute on load
+    checkWidth();
+
+    // Bind event listener
+    $(window).resize(checkWidth);
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 10) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
